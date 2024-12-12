1acdcc94254eb17243c53e69bf203e9abfd7f109
Nare33 <ulendaphaho@gmail.com>
2024-12-12 07:43:07 -0800

    Simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,7 @@
-principal = 1000
-rate = 0.05
-time = 3
+principal = int(input("1000"))
+rate = float(input("0.05"))
+time = float(input("3"))
+
 interest = (principal * rate * time) / 100
+
 print("Simple interest is:", interest)
