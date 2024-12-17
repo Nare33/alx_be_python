1e11c0bd7eef3a679612a8c715fd3aa7ad2dca1a
Nare33 <ulendaphaho@gmail.com>
2024-12-17 03:28:30 -0800

    Pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,20 +1,19 @@
 def main():
-    """Prompts user for size and draws a square pattern of asterisks."""
+    """Prompts user for size and draws a square pattern."""
 
     while True:
-        try:
-            size = int(input("Enter the size of the pattern (positive interger): "))
-            if size > 0:
-                break
-            else:
-                print("Please enter a positive integer.")
-        except ValueError:
-            print ("Invalid input. Please enter a number.")
+        size = int(input("Enter the size of the pattern: "))
+        if size > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+            except ValuueErroe:
+                print("Invalid input. please enter a number.")
 
-            for row in range(size):
-                for col in range(size):
-                    print("*", end="")
-                    print()
+                for row in range(size):
+                    for col in range(size):
+                        print ("*", end="")
+                        print()
 
-                    if __name == "__main__":
-                        main()
+                        if __name__ == "__main__":
+                            main()
